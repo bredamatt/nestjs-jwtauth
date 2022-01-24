38259b4c88ff8991f802608adb34a360a5b1842b
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-01-24 11:05:28 +0000

    Create barebrones auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    signupLocal(dto: AuthDto) {
+        // const newUser = this.prisma.user.create({
+        //     data: {
+        //         email: dto.email,
+        //         password: dto.password
+        //     }
+        // })
+    }
+    signinLocal() {}
+    logout() {}
+    refreshTokens() {}
+}
